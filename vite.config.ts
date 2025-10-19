feb9092a3d7b18c3241377fd3e0d50f5d1a75107
trungls1706 <trungls1706@gmail.com>
2025-10-19 23:26:50 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import federation from '@originjs/vite-plugin-federation';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'host-app',
+      name: "host-app",
       remotes: {
-        productsApp: 'http://localhost:5001/assets/remoteEntry.js',
-        cartApp: 'http://localhost:5002/assets/remoteEntry.js'
+        productsApp: process.env.PROD
+          ? "https://micro-frontend-remote-product.vercel.app/assets/remoteEntry.js"
+          : "http://localhost:5001/assets/remoteEntry.js",
+        cartApp: process.env.PROD
+          ? "https://micro-frontend-remote-cart.vercel.app/assets/remoteEntry.js"
+          : "http://localhost:5002/assets/remoteEntry.js",
       },
-      shared: ['react', 'react-dom']
-    })
+      shared: ["react", "react-dom"],
+    }),
   ],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   build: {
     modulePreload: false,
-    target: 'esnext',
+    target: "esnext",
     minify: false,
-    cssCodeSplit: false
-  }
+    cssCodeSplit: false,
+  },
 });
