60adabf59c666f4670ca81507de3827105f3672f
trungls1706 <trungls1706@gmail.com>
2025-10-20 12:24:00 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,33 @@ import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "host-app",
-      remotes: {
-        productsApp:"https://micro-frontend-remote-product.vercel.app/assets/remoteEntry.js",
-        cartApp:"https://micro-frontend-remote-cart.vercel.app/assets/remoteEntry.js",
-        // productsApp: import.meta.env.MODE === "production"
-        //   ? "https://micro-frontend-remote-product.vercel.app/assets/remoteEntry.js"
-        //   : "http://localhost:5001/assets/remoteEntry.js",
-        // cartApp: import.meta.env.MODE === "production"
-        //   ? "https://micro-frontend-remote-cart.vercel.app/assets/remoteEntry.js"
-        //   : "http://localhost:5002/assets/remoteEntry.js",
-      },
-      shared: ["react", "react-dom"],
-    }),
-  ],
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "host-app",
+        remotes: {
+          productsApp: isProd
+            ? "https://micro-frontend-remote-product.vercel.app/assets/remoteEntry.js"
+            : "http://localhost:5001/assets/remoteEntry.js",
+          cartApp: isProd
+            ? "https://micro-frontend-remote-cart.vercel.app/assets/remoteEntry.js"
+            : "http://localhost:5002/assets/remoteEntry.js",
+        },
+        shared: ["react", "react-dom"],
+      }),
+    ],
+    optimizeDeps: {
+      exclude: ["lucide-react"],
+    },
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
